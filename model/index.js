c3e17951e5ddc1f82dd0bca319a686fe44398b3a
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-12-20 22:54:57 +0200

    fix getContactById

--- model/index.js
+++ model/index.js
@@ -11,7 +11,7 @@ const listContacts = async () => {
 }
 
 const getContactById = async (contactId) => {
-  const [contactById] = contacts.find(contact => contact.id === contactId)
+  const contactById = contacts.find(contact => contact.id === contactId)
   return contactById
 }
 
